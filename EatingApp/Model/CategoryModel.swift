1d7ca07b542fde10c1efdec8fc648fb9110bb021
kangho Lee <ibcylon@naver.com>
2025-05-13 10:13:36 +0900

    Add: Tag, Category, Feed SwiftData Entity

--- EatingApp/Model/CategoryModel.swift
+++ EatingApp/Model/CategoryModel.swift
@@ -6,9 +6,27 @@
 //
 
 import Foundation
+import SwiftData
 
 struct CategoryModel: Identifiable {
     let id: Int
     let name: String
     let emoji: String
+}
+
+@Model
+class CategoryEntity {
+  @Attribute(.unique) var name: String
+  var emoji: String
+  
+  @Relationship(inverse: \FeedEntity.category)
+  var feeds = [FeedEntity]()
+  
+  @Relationship(inverse: \TagEntity.category)
+  var tags = [TagEntity]()
+  
+  init(name: String, emoji: String) {
+    self.name = name
+    self.emoji = emoji
+  }
 }
--- EatingApp/Model/TagModel.swift
+++ EatingApp/Model/TagModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftData
 
 struct TagModel: Identifiable {
     let id: Int
@@ -13,4 +14,23 @@ struct TagModel: Identifiable {
     let emoji: String
     let category_id: Int
     let use_yn: String
+}
+
+@Model
+class TagEntity {
+  @Attribute(.unique) var id: String
+  var name: String
+  var emoji: String
+  
+  @Relationship(inverse: \FeedEntity.tags)
+  var feeds = [FeedEntity]()
+  
+  var category: CategoryEntity
+  
+  init(name: String, emoji: String, category: CategoryEntity) {
+    self.name = name
+    self.emoji = emoji
+    self.id = UUID().uuidString
+    self.category = category
+  }
 }
