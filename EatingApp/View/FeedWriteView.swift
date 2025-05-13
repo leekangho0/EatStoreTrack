c2a2a01df7713cee07323cd2d4a3fd2a68602fd6
LeeChiHoon <dlclgns152615@gmail.com>
2025-05-13 10:28:59 +0900

    [Feat] tag를 입력받는 FeedWriteTagSelectView를 추가했습니다.
    Model 연결은 아직

--- EatingApp/View/FeedWriteView.swift
+++ EatingApp/View/FeedWriteView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct FeedWriteView: View {
-    var body: some View {
-        Text("글작성,편집뷰예요")
+  var body: some View {
+    VStack {
+      FeedWriteTagSelectView()
     }
+  } //: body
+}
+
+// MARK: FeedWriteTagSelectView
+struct FeedWriteTagSelectView: View {
+  
+  // TODO: SwiftData Tag값 load, Feed값 upLoad
+  
+  var body: some View {
+    ZStack {
+      RoundedRectangle(cornerRadius: 5)
+        .fill(.gray)
+        .frame(maxWidth: .infinity, minHeight: 150)
+      
+      Grid {
+        GridRow {
+          TagButtonView(emoji: "😊", tagName: "방긋")
+          TagButtonView(emoji: "😊", tagName: "방긋")
+        }
+      } //: Grid
+    } //: ZStack
+  } //: Body
+}
+
+// MARK: TagButtonView
+struct TagButtonView: View {
+  
+  var emoji: String
+  var tagName: String
+  
+  var body: some View {
+    Button {
+      
+    } label: {
+      VStack {
+        Text(emoji)
+        
+        Text(tagName)
+          .foregroundColor(.primary)
+      }
+    }
+  } //: body
 }
 
 #Preview {
-    FeedWriteView()
+  FeedWriteView()
 }
