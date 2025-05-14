089752d913c5ba6d75f971c64687fbbe3531191f
kangho Lee <ibcylon@naver.com>
2025-05-14 17:55:22 +0900

    Mod: FeedWrite UI

--- EatingApp/View/FeedWriteView.swift
+++ EatingApp/View/FeedWriteView.swift
@@ -7,171 +7,121 @@
 
 import SwiftUI
 import SwiftData
+import PhotosUI
 
 struct FeedWriteView: View {
-  
-  @Query var filteredTags: [TagEntity]
+  private static let placeholderText = "기록해주세요. 10자 이내"
   @Environment(\.dismiss) var dismiss
   @Environment(\.modelContext) private var modelContext
+  
+  @Query var filteredTags: [TagEntity]
+  
   @State private var content: String = ""
   @State private var showImagePicker = false
-  @State private var selectedImage: UIImage?
+  @State private var selectedImage: PhotosPickerItem?
+  @State private var selectedImageData: Data?
   @State private var selectedTagEntitys: [TagEntity]?
-  @State private var selectedTagIDs: Set<PersistentIdentifier> = [] // 선택된 tag를 관리하는 set
+  @State private var selectedTagIDs: Set<TagEntity> = []
   
-  let selectedCategoryEntity: Category
-
-  init(selectedCategoryEntity: Category) {
-    self.selectedCategoryEntity = selectedCategoryEntity
-    
-    let predicate = #Predicate<TagEntity> { tag in
-      tag.category == selectedCategoryEntity.rawValue
-    }
-    _filteredTags = Query(filter: predicate, sort: [SortDescriptor(\TagEntity.name)])
+  private enum Metric {
+    static let cornerRadius: CGFloat = 12
   }
-
+  
+  let category: Category
+  
+  init(category: Category) {
+    self.category = category
+    let categoryName: String = category.rawValue
+    let predicate = #Predicate<TagEntity> { tag in
+      tag.category == categoryName
+    }
+    
+    _filteredTags = Query(filter: predicate)
+  }
+  
   var body: some View {
-
     ScrollView {
-      VStack(spacing: 20) {
-        
-        HStack {
-          selectedCategoryEntity.icon
+      VStack(alignment: .leading) {
+        Text("카테고리")
+        HStack(spacing: 0) {
+          category.icon
+            .resizable()
+            .scaledToFit()
+            .frame(width: 50, height: 50)
           
-          Text(selectedCategoryEntity.rawValue)
+          Text(category.rawValue)
             .font(.headline)
-            .frame(maxWidth: .infinity)
-            .padding()
-            .background(Color.gray.opacity(0.2))
-            .cornerRadius(8)
             .padding(.horizontal)
         }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(.pBack1)
+        .clipShape(RoundedRectangle(cornerRadius: Metric.cornerRadius))
         
-        VStack {
-          Text("태그 선택")
-          
-          let columns = Array(repeating: GridItem(.flexible()), count: 4)
-          
-          LazyVGrid(columns: columns, spacing: 12) {
-            ForEach(filteredTags) { tag in
-              let isSelected = selectedTagIDs.contains(tag.persistentModelID)
-              
-              VStack(spacing: 2) {
-                Text(tag.emoji)
-                  .font(.largeTitle)
-                Text(tag.name)
-                  .font(.caption2)
-              }
-              .frame(width: 70, height: 70)
-              .background(isSelected ? Color.gray.opacity(0.15) : Color.clear)
-              // isSelected 값에 따라서 배경색을 정하는 코드입니다.
-              .cornerRadius(10)
-              .onTapGesture {
-                if isSelected {
-                  selectedTagIDs.remove(tag.persistentModelID)
-                  
-                } else {
-                  selectedTagIDs.insert(tag.persistentModelID)
-                }
-                
-                // selectedTagIDs에 포함된 ID들에 해당하는 TagEntity 객체들만 추려내서,
-                // selectedTagEntitys: [TagEntity]에 저장합니다.
-                selectedTagEntitys = filteredTags.filter {
-                  selectedTagIDs.contains($0.persistentModelID)
-                }
-              }
-            }
-          } //: LazyVGrid
-          .padding(.horizontal)
-        } //: VStack
-
-
-
-        Text("회고 작성")
-        ZStack(alignment: .topLeading) {
-          if content.isEmpty {
-            Text("내용을 입력하세요")
-              .foregroundColor(.gray)
-              .padding(.horizontal, 8)
-              .padding(.vertical, 12)
-            // TODO: 왜 안됨?
+        Text("태그")
+        TagGridLayout(tags: filteredTags, selectedTags: $selectedTagIDs)
+          .background(.pBack1)
+          .clipShape(RoundedRectangle(cornerRadius: Metric.cornerRadius))
+        
+        Text("회고 작성 20자")
+        
+        TextField("", text: $content, prompt: Text("지금을 기록해주세요. (10자 이내)"), axis: .vertical)
+          .padding()
+          .background {
+            Color.pBack1
           }
-          TextEditor(text: $content)
-            .padding(4)
-            .background(Color.clear)
-        }
-        .frame(height: 200)
-        .overlay(
-          RoundedRectangle(cornerRadius: 10)
-            .stroke(Color.gray.opacity(0.5))
-        )
-        .padding(.horizontal)
-
-        Spacer()
-
-
-
-        VStack {
+          .onChange(of: content) { oldValue, newValue in
+            content = String(content.prefix(20))
+          }
+          .clipShape(RoundedRectangle(cornerRadius: Metric.cornerRadius))
+        
           Text("사진")
-
-          Button {
-            showImagePicker = true
-          } label: {
-            ZStack {
-              if let image = selectedImage {
+          
+          PhotosPicker(selection: $selectedImage, matching: .images, photoLibrary: .shared()) {
+            Group {
+              if let selectedImageData, let image = UIImage(data: selectedImageData) {
                 Image(uiImage: image)
                   .resizable()
-                  .scaledToFill()
-                  .frame(height: 180)
-                  .clipped()
-                  .clipShape(RoundedRectangle(cornerRadius: 10))
+                  .scaledToFit()
+                  .clipShape(RoundedRectangle(cornerRadius: Metric.cornerRadius))
+                  .padding(20)
               } else {
-                Color.blue.opacity(0.2)
-                  .frame(height: 180)
-                  .clipShape(RoundedRectangle(cornerRadius: 10))
-
                 Image(systemName: "photo")
+                  .resizable()
+                  .scaledToFit()
+                  .frame(width: 100, height: 100)
+                  .tint(.primary)
               }
             }
           }
-
-        }
-
-        Button(action: {
-          // TODO: 저장 액션
-          dismiss()
-        }) {
-          Text("저장하기")
-            .font(.headline)
-            .frame(maxWidth: .infinity)
+          .frame(maxWidth: .infinity, minHeight: 200)
+          .background(Color.pBack1)
+          .clipShape(RoundedRectangle(cornerRadius: Metric.cornerRadius))
+        
+        Button {
+          saveFeed()
+        } label: {
+          Text("저장")
+            .font(.title3)
+            .bold()
+            .foregroundStyle(.black)
             .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(10)
-            .padding(.horizontal)
+            .frame(maxWidth: .infinity)
+            .background(selectedTagIDs.isEmpty ? .secondary : Color.pYellow)
+            .clipShape(RoundedRectangle(cornerRadius: Metric.cornerRadius))
         }
-
-      }
-      .padding()
-      .navigationBarBackButtonHidden(true)
-      .toolbar {
-        ToolbarItem(placement: .navigationBarLeading) {
-          Button {
-            self.saveFeed()
-            dismiss()
-          } label: {
-            Image(systemName: "chevron.left")
-          }
-        }
-
-        ToolbarItem(placement: .principal) {
-          Text("새 피드 작성")
-            .font(.headline)
-        }
+        .padding(.vertical)
+        .disabled(selectedTagIDs.isEmpty)
       }
     }
-    .sheet(isPresented: $showImagePicker) {
-      ImagePicker(image: $selectedImage)
+    .scrollIndicators(.hidden)
+    .padding()
+    .navigationBarBackButtonHidden(true)
+    .navigationTitle("새 피드 작성")
+    .task(id: selectedImage) {
+      if let data = try? await selectedImage?.loadTransferable(type: Data.self) {
+        selectedImageData = data
+      }
     }
   }
 }
@@ -180,36 +130,82 @@ extension FeedWriteView {
   
   // data에 저장되지않는 문제 있음
   func saveFeed() {
-    guard let selectedTagEntitys = selectedTagEntitys else { return }
-    guard let selectedImage = selectedImage, let imageData = selectedImage.jpegData(compressionQuality: 0.8) else { return }
-    // selectedImage 이미지 압축
+    guard let selectedTagEntitys else { return }
+    guard let selectedImageData else { return }
     
     let newFeed = FeedEntity(
       content: self.content,
-      category: self.selectedCategoryEntity,
+      category: self.category,
       tags: selectedTagEntitys,
-      image: imageData,
+      image: selectedImageData,
     )
     
     modelContext.insert(newFeed)
-    
-    do {
-      print("saved before")
-      try modelContext.save()
-      print("saved after")
-      
-      // debug
-      let savedFeeds = try modelContext.fetch(FetchDescriptor<FeedEntity>())
-      print("저장된 Feed 개수: \(savedFeeds.count)")
-    } catch {
-      print("\(error): saveFeed에서 발생한 에러")
-    }
+    //
+    //    do {
+    //      print("saved before")
+    //      try modelContext.save()
+    //      print("saved after")
+    //
+    //      // debug
+    //      let savedFeeds = try modelContext.fetch(FetchDescriptor<FeedEntity>())
+    //      print("저장된 Feed 개수: \(savedFeeds.count)")
+    //    } catch {
+    //      print("\(error): saveFeed에서 발생한 에러")
+    //    }
   }
   
 }
 
 #Preview {
   NavigationStack {
-//    FeedWriteView()
+    FeedWriteView(category: .food)
+      .modelContainer(try! ModelContainer.samples())
+  }
+}
+
+fileprivate struct TagGridLayout: View {
+  let columns = Array(repeating: GridItem(.flexible()), count: 4)
+  let tags: [TagEntity]
+  
+  @Binding var selectedTags: Set<TagEntity>
+  
+  var body: some View {
+    LazyVGrid(columns: columns) {
+      ForEach(tags) { tag in
+        let isSelected = selectedTags.contains(tag)
+        SelectableTagItem(tag: tag, isSelected: isSelected) {
+          if isSelected {
+            selectedTags.remove(tag)
+          } else {
+            selectedTags.insert(tag)
+          }
+        }
+      }
+    }
+    .padding()
+  }
+}
+
+struct SelectableTagItem: View {
+  let tag: TagEntity
+  let isSelected: Bool
+  let action: () -> Void
+  
+  var body: some View {
+    Button {
+      action()
+    } label: {
+      VStack(spacing: 2) {
+        Text(tag.emoji)
+          .font(.largeTitle)
+        Text(tag.name)
+          .font(.caption2)
+          .foregroundStyle(.black)
+      }
+      .frame(width: 70, height: 70)
+      .background(isSelected ? Color.gray.opacity(0.15) : Color.clear)
+      .cornerRadius(10)
+    }
   }
 }
